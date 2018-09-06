edbe19eb04956131d731411ea8528b7c1eb23121
snower <sujian199@gmail.com>
2018-09-06 18:47:43 +0800

    fix state padding

--- src/slock/db.go
+++ src/slock/db.go
@@ -10,23 +10,23 @@ const TIMEOUT_QUEUE_LENGTH int64 = 15
 const EXPRIED_QUEUE_LENGTH int64 = 15
 
 type LockDBState struct {
-    _padding0           [30]uint32
+    _padding0           [14]uint32
     LockCount           uint64
-    _padding1           [30]uint32
+    _padding1           [14]uint32
     UnLockCount         uint64
-    _padding2           [31]uint32
+    _padding2           [15]uint32
     LockedCount         uint32
-    _padding3           [31]uint32
+    _padding3           [15]uint32
     KeyCount            uint32
-    _padding4           [31]uint32
+    _padding4           [15]uint32
     WaitCount           uint32
-    _padding5           [31]uint32
+    _padding5           [15]uint32
     TimeoutedCount      uint32
-    _padding6           [31]uint32
+    _padding6           [15]uint32
     ExpriedCount        uint32
-    _padding7           [31]uint32
+    _padding7           [15]uint32
     UnlockErrorCount    uint32
-    _padding8           [31]uint32
+    _padding8           [15]uint32
 }
 
 type LockDB struct {
