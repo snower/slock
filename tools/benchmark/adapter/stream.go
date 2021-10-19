0c0c33a91a22828653c427936e54f2ce7d465883
snower <sujian199@gmail.com>
2021-10-20 00:37:00 +0800

    fix stream benchmark calc operator count

--- tools/benchmark/adapter/stream.go
+++ tools/benchmark/adapter/stream.go
@@ -112,7 +112,7 @@ func runStreamBenchmark(client net.Conn, count *uint32, wcount *uint32, maxCount
 			close(waiter)
 			return
 		}
-		atomic.AddUint32(wcount, 64)
+		atomic.AddUint32(wcount, 32)
 
 		err = readStreamAll(client, rbuf, 4096)
 		if err != nil {
@@ -120,7 +120,7 @@ func runStreamBenchmark(client net.Conn, count *uint32, wcount *uint32, maxCount
 			return
 		}
 
-		atomic.AddUint32(count, 64)
+		atomic.AddUint32(count, 32)
 		if *count > maxCount {
 			close(waiter)
 			return
